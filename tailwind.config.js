ea093e4b709809afcb95ebe579baeb8e7fe07d43
paul-thehuman <paul@thehumanco.org>
2025-08-05 17:43:14 +0100

    Complete professional build setup and fix security issue

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+  "./src/**/*.{html,js}",
+  "./public/**/*.{html,js}",
+  "./*.html",
+],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
